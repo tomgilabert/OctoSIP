1e967b3aec4c8e16ed7c7fcb5897756282f181ae
tomgilabert <tom.gilabert@gmail.com>
2026-04-09 12:10:23 +0200

    Update purge_old_events.sh

--- purge_old_events.sh
+++ purge_old_events.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deletes events older than 90 days and performs a vacuum
-source /opt/sipmon/config.conf
+source /opt/octosip/config.conf
 
 PGPASSWORD="$DB_PASSWORD" psql -h 127.0.0.1 -U "$DB_USER" -d "$DB_NAME" -c \
     "DELETE FROM sip_events WHERE ts < NOW() - make_interval(days=>90);"
